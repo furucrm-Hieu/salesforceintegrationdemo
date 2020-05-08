ae032938ae8b0f631fb010e063d1ba705461a67a
hieu.pham <hieu.pham@furucrm.com>
2020-05-08 15:01:08 +0700

    + Update apidoc

---
+++ public/doc/api_project.js
@@ -0,0 +1,33 @@
+define({
+  "name": "Rest Api SalesforceIntegrationDemo Document",
+  "version": "0.0.1",
+  "description": "",
+  "title": "Rest Api SalesforceIntegrationDemo Document",
+  "order": [
+    "OauthSalesforce",
+    "RefreshToken",
+    "GetToken",
+    "InsertProposal",
+    "UpdateProposal",
+    "DeleteProposal",
+    "InsertBudget",
+    "UpdateBudget",
+    "DeleteBudget",
+    "InsertProposalBudget",
+    "UpdateProposalBudget",
+    "DeleteProposalBudget"
+  ],
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-05-08T07:51:55.286Z",
+    "url": "http://apidocjs.com",
+    "version": "0.22.1"
+  }
+});
